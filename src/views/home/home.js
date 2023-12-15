1220bfa273bcfdb9b1fc13e085a1c6ba67d56806
suyash <suyashshinde76@gmail.com>
2023-12-15 14:04:16 +0530

    css added

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,31 +1,27 @@
 import React, { useState } from 'react';
 import I18n from '../../utils/I18n.js';
-import "./home.css";
+import './home.css'; 
 
 function Home() {
-//const userCount = 15;
+  const [language, setLang] = useState(localStorage.getItem('language') || 'en');
 
-const[language,setLang]=useState("en");
   return (
-    <div>
-      <h1>{I18n("welcomeMessage")}</h1>
-      <p>{I18n("normalMessage" )}</p>
-      <h3>{I18n( "greetingMessage")}</h3>
+    <div className="container">
+      <h1>{I18n('welcomeMessage')}</h1>
+      <p>{I18n('normalMessage')}</p>
+      <h3>{I18n('greetingMessage')}</h3>
 
       <select
-      defaultValue={localStorage.getItem("language")}
-      onChange={(e)=>{
-        localStorage.setItem("language",e.target.value);
-        window.location.reload();
-      }}>
-
-
-      <option value="mr">Marathi</option>
-      <option value="hi">Hindi</option>
-      <option value="en">English</option>
-      </select>  
-
-    {/*<p>{userCount} users are learning in this session</p>*/}
+        defaultValue={localStorage.getItem('language')}
+        onChange={(e) => {
+          localStorage.setItem('language', e.target.value);
+          window.location.reload();
+        }}
+      >
+        <option value="mr">Marathi</option>
+        <option value="hi">Hindi</option>
+        <option value="en">English</option>
+      </select>
     </div>
   );
 }
